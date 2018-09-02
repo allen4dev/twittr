444cbb5e84ee40522e7dcc2253822184aa07fbf1
Alan Aliaga <alanaliagadev@gmail.com>
2018-09-02 16:51:16 -0400

    [Add] async actions test base structure

--- src/modules/tweets/__tests__/asyncActions.spec.js
+++ src/modules/tweets/__tests__/asyncActions.spec.js
@@ -1,3 +1,26 @@
-it('should pass', () => {
-  expect(true).toBeTruthy();
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import moxios from 'moxios';
+
+import * as actions from './../actions';
+import * as actionTypes from './../actionTypes';
+import { INITIAL_STATE } from './../model';
+
+import axiosInstance from 'utils/axiosInstance';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('users module async actions', () => {
+  beforeEach(function() {
+    moxios.install(axiosInstance);
+  });
+
+  afterEach(function() {
+    moxios.uninstall(axiosInstance);
+  });
+
+  it('should pass', () => {
+    expect(true).toBeTruthy();
+  });
 });
