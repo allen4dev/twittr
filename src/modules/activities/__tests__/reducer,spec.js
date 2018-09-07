27c1d1c6513b5e16af4f9b8f0369ccc1b3cd7a71
allen4dev <alanaliagadev@gmail.com>
2018-09-06 23:15:51 -0400

    [Add] activities module - reducer - it should handle ADD_ACTIVITIES action

--- src/modules/activities/__tests__/reducer,spec.js
+++ src/modules/activities/__tests__/reducer,spec.js
@@ -1,3 +1,48 @@
-it('should pass', () => {
-  expect(true).toBeTruthy();
+import { INITIAL_STATE } from '../model';
+
+import * as actions from '../actions';
+
+import reducer from '../reducer';
+import entitiesReducer from '../reducer/entities';
+
+describe('activities', () => {
+  test('@INIT', () => {
+    expect(reducer(undefined, {})).toEqual(INITIAL_STATE);
+  });
+
+  describe('entities', () => {
+    const ENTITIES_STATE = INITIAL_STATE.entities;
+
+    it('should handle ADD_ACTIVITIES action', () => {
+      const activities = {
+        '1': { type: 'activities', id: '1' },
+        '2': { type: 'activities', id: '2' },
+      };
+
+      const nextState = entitiesReducer(
+        ENTITIES_STATE,
+        actions.addActivities(activities),
+      );
+
+      expect(nextState).toEqual({
+        ...ENTITIES_STATE,
+        ...activities,
+      });
+
+      const newActivities = {
+        '3': { type: 'activities', id: '3' },
+        '4': { type: 'activities', id: '4' },
+      };
+
+      const newState = entitiesReducer(
+        nextState,
+        actions.addActivities(newActivities),
+      );
+
+      expect(newState).toEqual({
+        ...nextState,
+        ...newActivities,
+      });
+    });
+  });
 });
